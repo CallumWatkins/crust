c4641f1a02dfb33826b35c327708485e0f5c34b7
Callum Watkins <callum@callumwatkins.com>
2022-07-14 01:36:04 +0100

    Add theme enum and dynamic theme binding

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+enum Theme {
+  Light = "LIGHT",
+  Dark = "DARK",
+}
+
+const theme = ref(Theme.Dark);
 </script>
 
 <template>
-  <div id="root" class="dark">
+  <div id="root" :class="theme.toLowerCase()">
     <RouterView />
   </div>
 </template>
