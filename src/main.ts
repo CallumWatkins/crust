0e6455979dcf51fe34d69d8f3138fb4c85d20f24
Richard <43828146+RichardEpure@users.noreply.github.com>
2022-07-14 22:32:44 +0100

    Add popper and edit hover stylings
    [skip ci]

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Popper from 'vue3-popper'
 
 createApp(App)
   .use(router)
+  .component("Popper", Popper)
   .mount('#app')
