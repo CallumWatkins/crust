8a95ba283917002a7300c6705ee159e4e2dc579d
Callum Watkins <callum@callumwatkins.com>
2022-07-14 23:37:42 +0100

    Add suspense

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,16 @@ const theme = ref(Theme.Dark);
 </script>
 
 <template>
-  <div id="root" class="is-flex is-flex-direction-column" :class="theme.toLowerCase()">
-    <div class="is-flex-shrink-0">
-      <WindowTitlebar @toggle-theme="theme = (theme === Theme.Dark ? Theme.Light : Theme.Dark)" />
+  <Suspense>
+    <div id="root" class="is-flex is-flex-direction-column" :class="theme.toLowerCase()">
+      <div class="is-flex-shrink-0">
+        <WindowTitlebar @toggle-theme="theme = (theme === Theme.Dark ? Theme.Light : Theme.Dark)" />
+      </div>
+      <div class="is-flex-grow-1">
+        <RouterView />
+      </div>
     </div>
-    <div class="is-flex-grow-1">
-      <RouterView />
-    </div>
-  </div>
+  </Suspense>
 </template>
 
 <style lang="scss">
