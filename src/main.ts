05c6adc00379cf501f3b6fcefef04dc7f05a530c
Callum Watkins <callum@callumwatkins.com>
2022-07-14 22:25:55 +0100

    Add Font Awesome

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faWindowMinimize, faWindowMaximize, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+fontAwesomeLibrary.add(faWindowMinimize, faWindowMaximize, faXmark)
+
 createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .use(router)
   .mount('#app')
