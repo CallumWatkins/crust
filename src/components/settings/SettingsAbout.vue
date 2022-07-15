ec64aae992cf09c8a056052b0f5cc3a1e524ef93
Callum Watkins <callum@callumwatkins.com>
2022-07-15 23:18:35 +0100

    Add settings about and update pages

---
+++ src/components/settings/SettingsAbout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { app, os } from '@tauri-apps/api';
+
+const appVersion = await app.getVersion();
+const tauriVersion = await app.getTauriVersion();
+const osArch = (await os.arch()).replace("x86_64", "x64");
+const osPlatform = await os.platform();
+const osType = await os.type();
+const osVersion = await os.version();
+</script>
+
+<template>
+  <div class="block">
+    <b>App</b>
+    <p>Version: {{ appVersion }}</p>
+    <p>Tauri Version: {{ tauriVersion }}</p>
+  </div>
+  <div class="block">
+    <b>OS</b>
+    <p>Type: {{ osType }}</p>
+    <p>Version: {{ osVersion }}</p>
+    <p>Platform: {{ osPlatform }}</p>
+    <p>Arch: {{ osArch }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/settings/SettingsUpdate.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { app, updater } from '@tauri-apps/api';
+
+const appVersion = await app.getVersion();
+</script>
+
+<template>
+  <div>
+    <div class="block">
+      <p>App version: {{ appVersion }}</p>
+      <b class="has-text-success">New version available: {{ appVersion }}</b>
+    </div>
+    <div class="block">
+      <button class="button">Check for updates</button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
