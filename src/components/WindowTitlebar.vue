8abc1b59e923342851851861ce84201f7c6b7482
Callum Watkins <callum@callumwatkins.com>
2022-07-16 19:51:27 +0100

    Refactor titlebar button styling

--- src/components/WindowTitlebar.vue
+++ src/components/WindowTitlebar.vue
@@ -13,22 +13,22 @@ appWindow.onResized(async (_) => {
 <template>
   <div class="titlebar" data-tauri-drag-region>
     <div class="titlebar__start">
-      <div class="window-control-button" @click="emit('toggleSettings')">
+      <div class="square-icon-button" @click="emit('toggleSettings')">
         <font-awesome-icon icon="fa-solid fa-bars" alt="settings" />
       </div>
-      <div class="window-control-button" @click="emit('toggleTheme')">
+      <div class="square-icon-button" @click="emit('toggleTheme')">
         <font-awesome-icon icon="fa-solid fa-bars" alt="settings" />
       </div>
     </div>
     <div class="titlebar__end">
-      <div class="window-control-button" @click="appWindow.minimize">
+      <div class="square-icon-button window-control" @click="appWindow.minimize">
         <font-awesome-icon icon="fa-solid fa-window-minimize" alt="minimize" />
       </div>
-      <div class="window-control-button" @click="appWindow.toggleMaximize">
+      <div class="square-icon-button window-control" @click="appWindow.toggleMaximize">
         <font-awesome-icon icon="fa-solid fa-window-restore" alt="restore" v-if="isMaximized" />
         <font-awesome-icon icon="fa-solid fa-window-maximize" alt="maximize" v-else />
       </div>
-      <div class="window-control-button" @click="appWindow.close">
+      <div class="square-icon-button window-control" @click="appWindow.close">
         <font-awesome-icon icon="fa-solid fa-xmark" size="lg" alt="close" />
       </div>
     </div>
@@ -38,38 +38,33 @@ appWindow.onResized(async (_) => {
 <style scoped lang="scss">
 .titlebar {
   border-bottom: 1px solid var(--border-color-dark);
-  display: flex;
-  flex-wrap: nowrap;
-  height: var(--titlebar-height);
-  justify-content: space-between;
-  user-select: none;
   z-index: 999;
+  height: 50px;
+  user-select: none;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
 
   &__start, &__end {
     display: flex;
     flex-wrap: nowrap;
   }
-}
 
-#titlebar-logo {
-  height: 100%;
-  max-width: unset;
-  padding: calc(var(--titlebar-height) * 0.1);
-}
+  .square-icon-button {
+    color: var(--text-color);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: var(--titlebar-height);
 
-.window-control-button {
-  align-items: center;
-  color: var(--text-color);
-  display: inline-flex;
-  height: 100%;
-  justify-content: center;
-  width: var(--titlebar-height);
+    &:hover {
+      background-color: var(--background-color-secondary);
+    }
 
-  &:hover {
-    background-color: var(--background-color-secondary);
-  }
-  &:last-child:hover {
-    background-color: rgb(244, 82, 42);
+    &.window-control:last-child:hover {
+      background-color: rgb(244, 82, 42);
+    }
   }
 }
 </style>
