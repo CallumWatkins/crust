d224611e74dab6193128ee6a34027de131bc34d2
Callum Watkins <callum@callumwatkins.com>
2022-07-17 21:25:40 +0100

    Add copy to clipboard button in SettingsAbout

--- src/components/settings/SettingsAbout.vue
+++ src/components/settings/SettingsAbout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { app, os } from '@tauri-apps/api';
+import { app, clipboard, os } from '@tauri-apps/api';
 
 const appVersion = await app.getVersion();
 const tauriVersion = await app.getTauriVersion();
@@ -7,6 +7,15 @@ const osArch = (await os.arch()).replace("x86_64", "x64");
 const osPlatform = await os.platform();
 const osType = await os.type();
 const osVersion = await os.version();
+
+async function copy_info() {
+  await clipboard.writeText(`App Version: ${appVersion}
+Tauri Version: ${tauriVersion}
+OS Type: ${osType}
+OS Version: ${osVersion}
+OS Platform: ${osPlatform}
+OS Arch: ${osArch}`);
+}
 </script>
 
 <template>
@@ -21,6 +30,9 @@ const osVersion = await os.version();
     <p>Version: {{ osVersion }}</p>
     <p>Platform: {{ osPlatform }}</p>
     <p>Arch: {{ osArch }}</p>
+  </div>
+  <div class="block">
+    <button class="button" @click="copy_info">Copy to clipboard</button>
   </div>
 </template>
 
