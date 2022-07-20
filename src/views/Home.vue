c8fac272ba76e42516faa738472e7fd63becd593
Callum Watkins <callum@callumwatkins.com>
2022-07-20 03:27:53 +0100

    Add host tab content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,6 +32,7 @@ const saved_connections: Connection[] = [
 ];
 
 let input_ip = ref("");
+let input_public = ref(false);
 
 function fill_input(conn: Connection) {
   input_ip.value = conn.ip;
@@ -39,6 +40,11 @@ function fill_input(conn: Connection) {
 
 function join() {
   const ip = input_ip.value;
+  // TODO
+}
+
+function host() {
+  const is_public = input_public.value;
   // TODO
 }
 
@@ -63,8 +69,35 @@ let current_tab = ref(Tabs.Join);
       </div>
       <Transition mode="out-in" name="quick-fade">
         <div v-if="current_tab === Tabs.Host">
+          <div class="field has-addons has-addons-centered mb-5">
+            <div class="level">
+              <div class="level-left">
+                <div class="level-item">
+                  <div class="level-item control is-flex is-align-content-center is-size-5">
+                    <div>Open session</div>
+                  </div>
+                </div>
+                <div class="level-item mr-3">
+                  <Popper content="Allow uninvited participants to join this session" placement="top" :hover="true" :interactive="false">
+                    <FontAwesomeIcon icon="fa-solid fa-circle-question" />
+                  </Popper>
                 </div>
               </div>
+              <div class="level-right">
+                <div class="level-item">
+                  <div class="control">
+                    <input id="make_public" type="checkbox" name="make_public" class="switch is-medium is-success" v-model="input_public">
+                    <label for="make_public"></label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="field is-grouped is-grouped-centered">
+            <div class="control">
+              <button class="button is-large is-rounded is-primary" @click="host">Host</button>
+            </div>
+          </div>
         </div>
         <div v-else-if="current_tab === Tabs.Join">
           <div class="field has-addons has-addons-centered mb-5">
@@ -119,6 +152,17 @@ let current_tab = ref(Tabs.Join);
   width: 100%;
   max-width: 400px;
 
+  #make_public + label {
+    padding-left: 3.75rem !important;
+
+    &::before {
+      top: 13px !important;
+    }
+
+    &::after {
+      top: 17px !important;
+    }
+  }
 }
 
 #logo {
