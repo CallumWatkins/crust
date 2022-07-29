3cd1566c2889397fe01649809db3f82f458dbc1b
Richard <43828146+RichardEpure@users.noreply.github.com>
2022-07-29 17:25:53 +0100

    Use SettingField component

--- src/components/settings/SettingsConnections.vue
+++ src/components/settings/SettingsConnections.vue
@@ -5,6 +5,7 @@ import { sort_by_property } from '../../helpers/sort';
 import PopupModal from '../PopupModal.vue';
 import ItemList from '../ItemList.vue';
 import { BasicSetting, setting_on_changed, connections_setting } from '../../model/Setting';
+import SettingField from './SettingField.vue';
 
 const { connections, add_connection, update_connection, delete_connection } = use_connections();
 const selected_conn: Ref<Connection | null> = ref(null);
@@ -65,8 +66,6 @@ const search = ref('');
 const validation_error: Ref<string | null> = ref(null);
 const modal_input_alias: Ref<string | null> = ref(null);
 const modal_input_ip = ref('');
-const show_alias_modal = ref(false);
-const show_ip_modal = ref(false);
 const show_add_modal = ref(false);
 const show_delete_modal = ref(false);
 
@@ -95,6 +94,10 @@ function open_alias_modal() {
 function open_ip_modal() {
   modal_input_ip.value = selected_conn.value!.ip;
   show_ip_modal.value = true;
+function selected_conn_changed(conn: Connection) {
+  selected_conn.value = conn;
+  alias_setting.value.value = conn.alias ?? '';
+  ip_setting.value.value = conn.ip;
 }
 
 function open_add_modal() {
@@ -106,26 +109,6 @@ function open_add_modal() {
 
 function open_delete_modal() {
   show_delete_modal.value = true;
-}
-
-function close_alias_modal(data: any) {
-  if (data === true) {
-    if (!modal_input_alias.value) {
-      selected_conn.value!.alias = null;
-    } else {
-      selected_conn.value!.alias = modal_input_alias.value.trim();
-    }
-    update_connection(selected_conn.value!);
-  }
-  show_alias_modal.value = false;
-}
-
-function close_ip_modal(data: any) {
-  if (data === true) {
-    selected_conn.value!.ip = modal_input_ip.value.trim();
-    update_connection(selected_conn.value!);
-  }
-  show_ip_modal.value = false;
 }
 
 function close_add_modal(data: any) {
@@ -223,29 +206,16 @@ function close_delete_modal(data: any) {
           </button>
         </div>
         <div v-if="selected_conn">
-          <div class="panel-block field-container">
-            <div>
-              <strong>Alias</strong>
-              <p>{{ selected_conn.alias ?? '-' }}</p>
-            </div>
-            <button
-              class="button"
-              @click="open_alias_modal"
-            >
-              Edit
-            </button>
-          </div>
-          <div class="panel-block field-container">
-            <div>
-              <strong>IP Address</strong>
-              <p>{{ selected_conn.ip }}</p>
-            </div>
-            <button
-              class="button"
-              @click="open_ip_modal"
-            >
-              Edit
-            </button>
+          <div class="panel-block is-block">
+            <SettingField
+              :setting="alias_setting"
+              no_value="-"
+              @changed="newVal => setting_on_changed(alias_setting, newVal)"
+            />
+            <SettingField
+              :setting="ip_setting"
+              @changed="newVal => setting_on_changed(ip_setting, newVal)"
+            />
           </div>
           <div class="panel-block is-flex is-justify-content-flex-end">
             <button
@@ -259,82 +229,6 @@ function close_delete_modal(data: any) {
       </div>
     </div>
   </div>
-  <PopupModal
-    v-slot="{ close }"
-    :is_open="show_alias_modal"
-    @closed="close_alias_modal"
-  >
-    <div class="box">
-      <p class="block title is-4">
-        Alias
-      </p>
-      <input
-        v-model="modal_input_alias"
-        class="block input"
-        type="text"
-        aria-label="Alias"
-      >
-      <p
-        v-if="validation_error !== null"
-        class="block has-text-danger"
-      >
-        {{ validation_error }}
-      </p>
-      <div class="block buttons">
-        <button
-          class="button is-success"
-          :disabled="validation_error !== null || modal_input_ip?.length === 0"
-          @click="close(true)"
-        >
-          Save
-        </button>
-        <button
-          class="button"
-          @click="close(false)"
-        >
-          Cancel
-        </button>
-      </div>
-    </div>
-  </PopupModal>
-  <PopupModal
-    v-slot="{ close }"
-    :is_open="show_ip_modal"
-    @closed="close_ip_modal"
-  >
-    <div class="box">
-      <p class="block title is-4">
-        IP Address
-      </p>
-      <input
-        v-model="modal_input_ip"
-        class="block input"
-        type="text"
-        aria-label="IP Address"
-      >
-      <p
-        v-if="validation_error !== null"
-        class="block has-text-danger"
-      >
-        {{ validation_error }}
-      </p>
-      <div class="block buttons">
-        <button
-          class="button is-success"
-          :disabled="validation_error !== null || modal_input_ip?.length === 0"
-          @click="close(true)"
-        >
-          Save
-        </button>
-        <button
-          class="button"
-          @click="close(false)"
-        >
-          Cancel
-        </button>
-      </div>
-    </div>
-  </PopupModal>
   <PopupModal
     v-slot="{ close }"
     :is_open="show_add_modal"
