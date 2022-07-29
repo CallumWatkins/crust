05c78821a0b300571b5b1e01aa7c3a6826b6cda1
Richard <43828146+RichardEpure@users.noreply.github.com>
2022-07-29 17:20:52 +0100

    Use connections settings in connections composable

--- src/composables/connections.ts
+++ src/composables/connections.ts
@@ -1,45 +1,8 @@
-import { ref, Ref } from 'vue';
+import { connections_setting } from '../model/Setting';
+import Connection from '../model/Connection';
 
-export interface Connection {
-  alias: string | null,
-  ip: string,
-  last_connected: Date | null,
-}
-
+// eslint-disable-next-line import/prefer-default-export
 export function use_connections() {
-  const connections: Ref<Connection[]> = ref([
-    {
-      alias: 'John Doe',
-      ip: '206.15.168.235',
-      last_connected: new Date('2022-01-16'),
-    },
-    {
-      alias: 'James Smith',
-      ip: '3.66.149.79',
-      last_connected: new Date('2022-03-22'),
-    },
-    {
-      alias: null,
-      ip: '62.109.37.164',
-      last_connected: null,
-    },
-    {
-      alias: null,
-      ip: '34.61.123.222',
-      last_connected: null,
-    },
-    {
-      alias: null,
-      ip: '215.4.207.51',
-      last_connected: new Date('2022-04-01'),
-    },
-    {
-      alias: 'Charles Smith',
-      ip: '39.6.121.89',
-      last_connected: new Date('2022-05-12'),
-    },
-  ]);
-
   const recent_connections: Ref<Connection[]> = ref([
     {
       alias: 'Raveena Adkins',
@@ -57,33 +20,25 @@ export function use_connections() {
   ]);
 
   function find_connection(conn: Connection) {
-    return connections.value.findIndex((connection) => connection.ip === conn.ip);
+    return connections_setting.value.findIndex((connection) => connection.ip === conn.ip);
   }
 
   function add_connection(conn: Connection) {
-    connections.value.push(conn);
+    connections_setting.value.push(conn);
+    connections_setting.save();
   }
 
   function delete_connection(conn: Connection) {
     const index = find_connection(conn);
     if (index >= 0) {
-      connections.value.splice(index, 1);
-    }
-  }
-
-  function update_connection(conn: Connection) {
-    const index = find_connection(conn);
-    if (index >= 0) {
-      connections.value[index].alias = conn.alias;
-      connections.value[index].ip = conn.ip;
+      connections_setting.value.splice(index, 1);
+      connections_setting.save();
     }
   }
 
   return {
-    connections,
     recent_connections,
     add_connection,
     delete_connection,
-    update_connection,
   };
 }
