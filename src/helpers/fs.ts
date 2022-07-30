d1d85765ad0aa06a4d0207abe0618de893081342
Callum Watkins <callum@callumwatkins.com>
2022-07-30 01:09:57 +0100

    Add binary file methods to fs helper

--- src/helpers/fs.ts
+++ src/helpers/fs.ts
@@ -1,4 +1,6 @@
-import { readDir, readTextFile, BaseDirectory, FileEntry, createDir, writeTextFile } from '@tauri-apps/api/fs';
+import {
+  readDir, readTextFile, BaseDirectory, FileEntry, createDir, writeTextFile, readBinaryFile, writeBinaryFile, BinaryFileContents,
+} from '@tauri-apps/api/fs';
 
 export { BaseDirectory };
 
@@ -47,4 +49,33 @@ export async function read_text_file(base_dir: BaseDirectory, path: string, file
 export async function write_text_file(base_dir: BaseDirectory, path: string, file_name: string, contents: string) {
   await createDir(path, { dir: base_dir, recursive: true });
   await writeTextFile(path + file_name, contents, { dir: base_dir });
+}
+
+/**
+ * Attemts to read a binary file. Returns null if the file does not exist.
+ *
+ * @export
+ * @param {BaseDirectory} base_dir
+ * @param {string} path e.g. 'folder/subfolder'.
+ * @param {string} file_name e.g. 'file.txt'.
+ * @return The content of the file, or null if the file does not exist.
+ */
+export async function read_binary_file(base_dir: BaseDirectory, path: string, file_name: string): Promise<Uint8Array | null> {
+  return await file_exists(base_dir, path, file_name)
+    ? readBinaryFile(path + file_name, { dir: base_dir })
+    : null;
+}
+
+/**
+ * Writes to a binary file. Creates the directory and file if either do not already exist.
+ *
+ * @export
+ * @param {BaseDirectory} base_dir
+ * @param {string} path e.g. 'folder/subfolder'.
+ * @param {string} file_name e.g. 'file.txt'.
+ * @param {BinaryFileContents} contents The contents to write.
+ */
+export async function write_binary_file(base_dir: BaseDirectory, path: string, file_name: string, contents: BinaryFileContents) {
+  await createDir(path, { dir: base_dir, recursive: true });
+  await writeBinaryFile(path + file_name, contents, { dir: base_dir });
 }
