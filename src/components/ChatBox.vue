5a463fb5a2f45326c16f4ca0644ff04ec2b16b8d
Callum Watkins <callum@callumwatkins.com>
2022-07-31 21:10:58 +0100

    Add new message validation

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Ref, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import Message from '../model/Message';
 import ChatMessage from './ChatMessage.vue';
@@ -28,6 +29,35 @@ function is_new_group(previousMessage: Message, message: Message): boolean {
   if (previousMessage.sender !== message.sender) return true;
   if (time_difference_seconds(previousMessage.date, message.date) > 10 * 60) return true;
   return false;
+}
+
+const message_input = ref('');
+const message_validation_error: Ref<string | null> = ref(null);
+const message_validation_shake = ref(false);
+
+function validate_message(message: string): string | null {
+  if (message.length > 5000) return `Message length is too long (${message.length} / 5000)`;
+  return null;
+}
+
+function show_message_validation_error(error: string) {
+  message_validation_error.value = error;
+  message_validation_shake.value = true;
+}
+
+function send_message() {
+  const message = message_input.value.trim();
+  if (message.length === 0) return;
+
+  const validation_result = validate_message(message);
+  if (validation_result !== null) {
+    show_message_validation_error(validation_result);
+    return;
+  }
+  message_validation_error.value = null;
+
+  // TODO: send message
+  message_input.value = '';
 }
 </script>
 
@@ -81,11 +111,21 @@ function is_new_group(previousMessage: Message, message: Message): boolean {
     </div>
     <div class="chat-input">
       <input
+        v-model="message_input"
         class="input"
+        :class="{ 'shake': message_validation_shake }"
         type="text"
         placeholder="Message"
         aria-label="Message"
+        @animationend="message_validation_shake = false"
+        @keypress.enter="send_message"
       >
+      <div
+        v-if="message_validation_error !== null"
+        class="chat-input__error"
+      >
+        {{ message_validation_error }}
+      </div>
     </div>
   </div>
 </template>
@@ -144,11 +184,42 @@ function is_new_group(previousMessage: Message, message: Message): boolean {
 }
 
 .chat-input {
+  position: relative;
   padding: 0 0.5rem 0.5rem;
+
+  &__error {
+    position: absolute;
+    bottom: 100%;
+    left: 1rem;
+    font-size: 0.85rem;
+    color: var(--text-color-danger);
+    background-color: var(--background-color);
+    padding: 0.2em 0.5em 0;
+    border-top-left-radius: var(--small-radius);
+    border-top-right-radius: var(--small-radius);
+  }
 
   input {
     border: 0;
     background-color: var(--background-color-darker);
+
+    &.shake {
+      animation-name: shake;
+      animation-duration: 0.4s;
+    }
+  }
+}
+
+@keyframes shake {
+  10%,
+  50%,
+  90% {
+    transform: translate3d(-3px, 0, 0);
+  }
+
+  30%,
+  70% {
+    transform: translate3d(3px, 0, 0);
   }
 }
 </style>
