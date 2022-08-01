87fc300c039e466794ac3b9e8ed1d5183b83a206
Richard <43828146+RichardEpure@users.noreply.github.com>
2022-08-01 18:37:47 +0100

    Add UserProfile component

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref, Ref } from 'vue';
+import { User } from '../composables/users';
+import UserAvatar from './UserAvatar.vue';
+
+interface Props {
+  user: User;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="user-profile p-5">
+    <UserAvatar class="avatar mb-5" />
+    <p class="is-size-4">
+      <strong>{{ props.user.connection.alias ?? props.user.username ?? props.user.connection.ip }}</strong>
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user-profile {
+  background-color: var(--background-color-darker);
+  border-radius: var(--large-radius);
+  box-shadow: $dropdown-content-shadow;
+  width: 300px;
+}
+
+.avatar {
+  width: 86px;
+}
+</style>
