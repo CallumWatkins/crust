99ceb537a046b5c42d7b9200a4e3d2041f35afc8
Richard <43828146+RichardEpure@users.noreply.github.com>
2022-08-01 19:49:42 +0100

    Add ChatBox & UserList components in RoomView

--- src/views/RoomView.vue
+++ src/views/RoomView.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
 import ChatBox from '../components/ChatBox.vue';
+import UserList from '../components/UserList.vue';
 </script>
 
 <template>
-  <ChatBox />
+  <div class="room">
+    <ChatBox />
+    <div class="user-list-wrapper">
+      <UserList />
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.room {
+  display: flex;
+  height: 100%;
+}
+
+.user-list-wrapper {
+  border-left: 1px solid var(--border-color-dark);
+}
+</style>
