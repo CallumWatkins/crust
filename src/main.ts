651009f34d9cb2732ae807282d934c60e5abd687
Richard <43828146+RichardEpure@users.noreply.github.com>
2022-08-01 18:37:22 +0100

    Move 'reflect-metadata' import

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import devtools from '@vue/devtools';
 import { createApp } from 'vue';
+import 'reflect-metadata';
 import Popper from 'vue3-popper';
 
 import { library as font_awesome_library } from '@fortawesome/fontawesome-svg-core';
@@ -9,7 +10,8 @@ import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome
 import {
   faWindowMinimize, faWindowMaximize, faWindowRestore, faXmark,
   faBars, faCaretLeft, faChevronDown, faCircleCheck, faLightbulb,
-  faCircle, faUser, faCircleQuestion, faMagnifyingGlass, faPlus, faSort,
+  faCircle, faUser, faCircleQuestion, faMagnifyingGlass, faPlus,
+  faSort, faCrown,
 } from '@fortawesome/free-solid-svg-icons';
 
 import App from './App.vue';
@@ -18,7 +20,8 @@ import router from './router';
 font_awesome_library.add(
   faWindowMinimize, faWindowMaximize, faWindowRestore, faXmark,
   faBars, faCaretLeft, faChevronDown, faCircleCheck, faLightbulb,
-  faCircle, faUser, faCircleQuestion, faMagnifyingGlass, faPlus, faSort,
+  faCircle, faUser, faCircleQuestion, faMagnifyingGlass, faPlus,
+  faSort, faCrown,
 );
 
 createApp(App)
